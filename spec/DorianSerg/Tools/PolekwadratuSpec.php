b4542a6c01b7d8e8286805d782fb3557608979a8
DorianSerg <dorian.sergiel@gmail.com>
2015-05-29 05:28:48 +0200

    DorianSerg/Polekwadratu: pole kwadratu-metody

--- spec/DorianSerg/Tools/PolekwadratuSpec.php
+++ spec/DorianSerg/Tools/PolekwadratuSpec.php
@@ -7,8 +7,18 @@ use Prophecy\Argument;
 
 class PolekwadratuSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+function it_is_initializable()
     {
         $this->shouldHaveType('DorianSerg\Tools\Polekwadratu');
+    }
+    
+     function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+    }
+    
+    function it_should_calculate_polekwadratu()
+    {
+        $this->setA(5)->polekwadratu()->shouldReturn(25);
     }
 }
